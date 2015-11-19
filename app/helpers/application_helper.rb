d7fd7e8c458f45e8791a1a84f1d22b5052679665
John Clark <johngclark36@gmail.com>
2015-11-19 16:02:42 -0500

    Add helper for displaying lyrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def ugly_lyrics(lyrics)
+    result = ""
+    list = lyrics.split("\r\n")
+    list.each do |line|
+      result += "&#9835;" + html_escape(line)
+    end
+    result.html_safe
+  end
 end
